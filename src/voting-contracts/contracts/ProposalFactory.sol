0b2a47e3e198233904e8cdf3251a6546be533bee
Saad S <saad.shafiq@tintash.com>
2022-09-12 18:51:08 +0500

    refactor: factory contract improvements

--- src/voting-contracts/contracts/ProposalFactory.sol
+++ src/voting-contracts/contracts/ProposalFactory.sol
@@ -3,44 +3,59 @@ pragma solidity 0.8.15;
 
 // TODO: import relative path, probably two package.json
 import "../node_modules/@openzeppelin/contracts/proxy/Clones.sol";
+import "../node_modules/@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./Proposal.sol";
 
-contract ProposalFactory {
+contract ProposalFactory is ReentrancyGuard {
     address private proposalImplementation;
+    
+    event ProposalCreated(string indexed guid, address indexed clonedProposal);
 
-    event CreatedProposal(string guid, address clonedProposal);
-
-    constructor(address _implementation) {
+    constructor(address implementation_) ReentrancyGuard() {
         // proposalImplementation = address(new Proposal());
-        proposalImplementation = _implementation;
+        proposalImplementation = implementation_;
     }
 
     /** 
         create a new proposal with provided details
     */
     function createProposal(
-        address _strategiesContract,
-        string memory _guid,
-        string memory _title,
-        string memory _uri,
-        string[] memory _options,
-        uint256 _startOffset,
-        uint256 _stopOffset,
-        Proposal.VotingTypes _votingType
-    ) external returns (address) {
+        address strategiesContract,
+        string memory guid,
+        string memory title,
+        string memory uri,
+        string[] memory options,
+        uint256 startOffset,
+        uint256 stopOffset,
+        Proposal.VotingTypes votingType
+    ) 
+    external 
+    nonReentrant
+    returns (address) {
+        /** it is advised to check validity of passed contract addresses,
+            but there is no way to be 100% sure if the passed address is
+            actually a contract address or not. Discussed here ...
+            https://stackoverflow.com/questions/37644395/how-to-find-out-if-an-ethereum-address-is-a-contract
+            
+            For this contract, caller (frontend) is reponsible for passing 
+            valid addresses. A third party attempt to call this contract will 
+            return a proposal contract address, which will not be shown on 
+            frontend or handled by backend. This is because deploying strategy 
+            contract and creating a proposal are atomic operation on frontend. 
+        */
         address clonedProposal = Clones.clone(proposalImplementation);
         Proposal(clonedProposal).init(
             msg.sender,
-            _strategiesContract,
-            _guid,
-            _title,
-            _uri,
-            _options,
-            _startOffset,
-            _stopOffset,
-            _votingType
+            strategiesContract,
+            guid,
+            title,
+            uri,
+            options,
+            startOffset,
+            stopOffset,
+            votingType
         );
-        emit CreatedProposal(_guid, clonedProposal);
+        emit ProposalCreated(guid, clonedProposal);
         return clonedProposal;
     }
 }
